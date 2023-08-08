658206d22a56e7e0702fcd0c750383becac7acf6
davi <davitpontas@gmail.com>
2023-08-08 13:51:15 -0300

    Botões adicionados e melhor tratamento da UI

--- Front/FullStack-App/src/app/eventos/eventos.component.ts
+++ Front/FullStack-App/src/app/eventos/eventos.component.ts
@@ -7,22 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent {
-  public eventos: any;
+  public eventos: any = [];
+  widthImg: number = 150;
+  marginImg: number = 2;
+  hideImg: boolean = false;
+  private _filtroLista: string = '';
+  public eventosFiltrados: any = [];
 
-  constructor(private http: HttpClient){
-    
+  public get filtroLista(): string {
+    return this._filtroLista;
   }
-  
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { local: any; tema: string }) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
+  showImg() {
+    this.hideImg = !this.hideImg;
+  }
+
+  constructor(private http: HttpClient) {}
+
   ngOnInit() {
     this.getEventos();
   }
- 
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/evento').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
-    )
-    
+      (response) => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
+      (error) => console.log(error)
+    );
   }
 }
