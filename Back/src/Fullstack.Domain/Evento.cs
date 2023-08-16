282bc8135d55e6c8986e3df107fee86250422347
davi <davitpontas@gmail.com>
2023-08-16 15:43:12 -0300

    DTOs Data Annotations e Mapamento concluido

--- Back/src/Fullstack.Domain/Evento.cs
+++ Back/src/Fullstack.Domain/Evento.cs
@@ -1,4 +1,7 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Fullstack.Domain;
+
 public class Evento
 {
     public int Id { get; set; }
@@ -7,10 +10,9 @@ public class Evento
     public string Tema { get; set; }
     public int QtdPessoas { get; set; }
     public string ImagemUrl { get; set; }
-    public int Telefone { get; set; }
+    public string Telefone { get; set; }
     public string Email { get; set; } 
     public IEnumerable<Lote> Lotes { get; set; }
     public IEnumerable<RedeSocial> RedeSociais { get; set; }
-    public IEnumerable<Palestrante> Palestrantes { get; set; }
     public IEnumerable<PalestranteEvento> PalestrantesEventos { get; set; }
 }
